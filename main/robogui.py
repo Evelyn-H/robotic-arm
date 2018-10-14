8e406641b36c4da1f652335f766e3c6cb9152f4c
Eric Risbakk <e.risbakk@student.maastrichtuniversity.nl>
2018-10-14 18:05:04 +0200

    Can now make a single consecutive line.

--- main/robogui.py
+++ main/robogui.py
@@ -9,6 +9,33 @@ from tkinter import *
 from tkinter import ttk
 root = Tk()
 
+# Global variables
+bg = 0
+robo = 0
+cam = 0
+bgoffsetx, bgoffsety = 50, 50
+bgx, bgy = 500, 300
+curLine = None
+
+# Methods
+def xy(event):
+    global lastx, lasty
+    lastx, lasty = canvas.canvasx(event.x), canvas.canvasy(event.y)
+
+def addLine(event):
+    xy(event)
+    global lastx, lasty, curLine
+    if (curLine is None):
+        curLine = canvas.create_line((lastx, lasty, lastx, lasty), width=3)
+    else:
+        x,y = canvas.canvasx(event.x), canvas.canvasy(event.y)
+        extendLine(curLine, x, y)
+        
+
+# Adds another point to the given line
+def extendLine(line, x, y):
+    canvas.coords(line, canvas.coords(line)+[x,y])
+
 # Creating window essentials
 h = ttk.Scrollbar(root, orient=HORIZONTAL)
 v = ttk.Scrollbar(root, orient=VERTICAL)
@@ -40,8 +67,20 @@ newLine.grid(column=0,row=0)
 editLine.grid(column=1, row=0)
 deleteLine.grid(column=2,row=0)
 addPoint.grid(column=3, row=0)
-deletePoint.grid(column=4, row=0)
+insertPoint.grid(column=4, row=0)
+deletePoint.grid(column=5, row=0)
 
+# Bindings
+canvas.bind("<Button-1>", addLine)
+
+# Base stuff, like white rectangle bg and ish-robot position.
+bg = canvas.create_rectangle((bgoffsetx,bgoffsety, bgoffsetx+bgx, bgoffsety+bgy), 
+                             fill='white', tags=('bg'))
+robo = canvas.create_rectangle((bgoffsetx + (bgx/3), 5, bgoffsetx+bgx-(bgx/3), bgoffsety-5), 
+                               fill='black', tags =('robo'))
+cam = canvas.create_rectangle((bgoffsetx + (bgx/2) - (bgx/20), bgoffsety+bgy+5,
+                               bgoffsetx + (bgx/2) + (bgx/20), bgoffsety+bgy+5+(bgoffsety)),
+                              fill='grey', tags=('cam'))
 
 # Begin thingie.
 root.mainloop()
