99e0b77cb2a0b21223e46c69826b587680b9b1fd
Evelyn_H <manu.hobert@gmail.com>
2019-01-11 17:01:47 +0100

    Pose estimation works!

---
+++ vision/calibrate_cam.py
@@ -0,0 +1,133 @@
+import numpy as np
+import cv2
+import glob
+
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((7 * 7, 3), np.float32)
+objp[:, :2] = np.mgrid[0:7, 0:7].T.reshape(-1, 2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = []  # 3d point in real world space
+imgpoints = []  # 2d points in image plane.
+
+# images = glob.glob('*.jpg')
+cam = cv2.VideoCapture(0)
+cam.set(3, 960); cam.set(4, 720)
+
+ret, mtx, dist, rvecs, tvecs = None, None, None, None, None
+
+mtx = np.array(
+    [[1.21590122e+03, 0.00000000e+00, 3.75456517e+02],
+     [0.00000000e+00, 1.17248719e+03, 3.56379910e+02],
+     [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]]
+)
+dst = np.array([[0.11245815, 0.30009981, 0.0081315, -0.03802704, -0.6149271]])
+
+def put_position_orientation_value_to_frame(frame, translation, rotation):
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
+    cv2.putText(frame,'position(cm)',(10,30), font, 0.7,(0,255,0),1,cv2.LINE_AA)
+    cv2.putText(frame,'x:'+str(round(translation[0][0],2)),(250,30), font, 0.7,(0,0,255),2,cv2.LINE_AA)
+    cv2.putText(frame,'y:'+str(round(translation[1][0],2)),(350,30), font, 0.7,(0,0,255),2,cv2.LINE_AA)
+    cv2.putText(frame,'z:'+str(round(translation[2][0],2)),(450,30), font, 0.7,(0,0,255),2,cv2.LINE_AA)
+
+    cv2.putText(frame,'orientation(degree)',(10,60), font, 0.7,(0,255,0),1,cv2.LINE_AA)
+    cv2.putText(frame,'x:'+str(round(rotation[0][0] * 180. / np.pi,2)),(250,60), font, 0.7,(0,0,255),2,cv2.LINE_AA)
+    cv2.putText(frame,'y:'+str(round(rotation[1][0] * 180. / np.pi,2)),(350,60), font, 0.7,(0,0,255),2,cv2.LINE_AA)
+    cv2.putText(frame,'z:'+str(round(rotation[2][0] * 180. / np.pi,2)),(450,60), font, 0.7,(0,0,255),2,cv2.LINE_AA)
+
+    return frame
+
+
+# for fname in images:
+while True:
+    # img = cv2.imread(fname)
+    _, img = cam.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    if not mtx is None:
+        axis = np.float32([[3, 0, 0], [0, 3, 0], [0, 0, -3]]).reshape(-1, 3)
+
+        # img = cv2.imread(fname)
+        # gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        ret, corners = cv2.findChessboardCorners(gray, (7,7),None)
+
+        if ret is True:
+            corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+            print(corners2)
+            # Find the rotation and transl(ation vectors.
+            _, rvecs, tvecs, inliers = cv2.solvePnPRansac(objp, corners2, mtx, dist)
+            print(repr(objp))
+
+            # project 3D points to image plane
+            imgpts, jac = cv2.projectPoints(axis, rvecs, tvecs, mtx, dist)
+
+            def draw(img, corners, imgpts):
+                corner = tuple(corners[0].ravel())
+                img = cv2.line(img, corner, tuple(imgpts[0].ravel()), (255,0,0), 5)
+                img = cv2.line(img, corner, tuple(imgpts[1].ravel()), (0,255,0), 5)
+                img = cv2.line(img, corner, tuple(imgpts[2].ravel()), (0,0,255), 5)
+                return img
+
+            img = draw(img, corners2, imgpts)
+            img = put_position_orientation_value_to_frame(img, tvecs, rvecs)
+
+    cv2.imshow('cam', img)
+
+    k = cv2.waitKey(1) & 0xFF
+    if k == ord('i'):
+        # Find the chess board corners
+        ret, corners = cv2.findChessboardCorners(gray, (7, 7), None)
+
+        # If found, add object points, image points (after refining them)
+        print(ret)
+        if ret is True:
+            objpoints.append(objp)
+
+            corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+            imgpoints.append(corners2)
+
+            # Draw and display the corners
+            img = cv2.drawChessboardCorners(img, (7, 7), corners2, ret)
+            cv2.imshow('cam', img)
+            while not cv2.waitKey(200) & 0xFF == ord('i'):
+                pass
+
+    if k == ord('p'):
+        # ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+        ret, mtx, dist, _, _ = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+        print(
+            ret,
+            mtx,
+            dist,
+            # rvecs,
+            # tvecs,
+            sep='\n'
+        )
+
+        # undistort
+        # img = cv2.imread('left12.jpg')
+        h, w = img.shape[:2]
+        newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+
+        dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+
+        x, y, w, h = roi
+        dst = dst[y:y + h, x:x + w]
+        # cv2.imwrite('calibresult.png',dst)
+
+        # # error
+        # tot_error = 0
+        # for i in range(len(objpoints)):
+        #     imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+        #     error = cv2.norm(imgpoints[i],imgpoints2, cv2.NORM_L2)/len(imgpoints2)
+        #     tot_error += error
+        #
+        # print("total error: ", tot_error/len(objpoints))
+
+        cv2.imshow('cam', img)
+        while not cv2.waitKey(200) & 0xFF == ord('p'):
+            pass
--- vision/featuretest.py
+++ vision/featuretest.py
@@ -21,8 +21,57 @@ matcher = cv2.BFMatcher(cv2.NORM_HAMMING)
 cam = cv2.VideoCapture(0)
 cam.set(3, 960); cam.set(4, 720)
 
+mtx = np.array(
+[[1.21590122e+03, 0.00000000e+00, 3.75456517e+02],
+[0.00000000e+00, 1.17248719e+03, 3.56379910e+02],
+[0.00000000e+00, 0.00000000e+00, 1.00000000e+00]]
+)
+dist = np.array([[0.11245815, 0.30009981, 0.0081315, -0.03802704, -0.6149271]])
+
 avg_len = 5
 avg_buffer = deque(maxlen=avg_len)
+
+def find_pose(img, corners):
+    axis = np.float32([[3, 0, 0], [0, 3, 0], [0, 0, -3]]).reshape(-1, 3)
+
+    # corner points of image
+    objp = np.array([
+        [0, 0, 0],
+        [0, 6, 0],
+        [6, 6, 0],
+        [6, 0, 0],
+    ], dtype=np.float32)
+    # Find the rotation and translation vectors.
+    _, rvecs, tvecs, inliers = cv2.solvePnPRansac(objp, corners, mtx, dist)
+
+    # project 3D points to image plane
+    imgpts, jac = cv2.projectPoints(axis, rvecs, tvecs, mtx, dist)
+
+    def draw(img, corners, imgpts):
+        corner = tuple(corners[0].ravel())
+        print(imgpts)
+        img = cv2.line(img, corner, tuple(imgpts[0].ravel()), (255,0,0), 5)
+        img = cv2.line(img, corner, tuple(imgpts[1].ravel()), (0,255,0), 5)
+        img = cv2.line(img, corner, tuple(imgpts[2].ravel()), (0,0,255), 5)
+        return img
+
+    def put_position_orientation_value_to_frame(frame, translation, rotation):
+        font = cv2.FONT_HERSHEY_SIMPLEX
+
+        cv2.putText(frame,'position(cm)',(10,30), font, 0.7,(0,255,0),1,cv2.LINE_AA)
+        cv2.putText(frame,'x:'+str(round(translation[0][0],2)),(250,30), font, 0.7,(0,0,255),2,cv2.LINE_AA)
+        cv2.putText(frame,'y:'+str(round(translation[1][0],2)),(350,30), font, 0.7,(0,0,255),2,cv2.LINE_AA)
+        cv2.putText(frame,'z:'+str(round(translation[2][0],2)),(450,30), font, 0.7,(0,0,255),2,cv2.LINE_AA)
+
+        cv2.putText(frame,'orientation(degree)',(10,60), font, 0.7,(0,255,0),1,cv2.LINE_AA)
+        cv2.putText(frame,'x:'+str(round(rotation[0][0] * 180. / np.pi,2)),(250,60), font, 0.7,(0,0,255),2,cv2.LINE_AA)
+        cv2.putText(frame,'y:'+str(round(rotation[1][0] * 180. / np.pi,2)),(350,60), font, 0.7,(0,0,255),2,cv2.LINE_AA)
+        cv2.putText(frame,'z:'+str(round(rotation[2][0] * 180. / np.pi,2)),(450,60), font, 0.7,(0,0,255),2,cv2.LINE_AA)
+
+        return frame
+
+    img = draw(img, corners, imgpts)
+    img = put_position_orientation_value_to_frame(img, tvecs, rvecs)
 
 while True:
     _, frame = cam.read()
@@ -71,23 +120,25 @@ while True:
                     x_avg = sum(sorted(x for x, y in p)[num_outliers:-num_outliers]) / (avg_len - 2*num_outliers)
                     y_avg = sum(sorted(y for x, y in p)[num_outliers:-num_outliers]) / (avg_len - 2*num_outliers)
                     dst.append([[x_avg, y_avg]])
-                print(dst)
 
-            img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+            dst = np.array(dst, dtype=np.float32)
+            frame = cv2.polylines(frame,[np.int32(dst)],True,(255,0,255),3, cv2.LINE_AA)
+            # print(repr(dst))
+            find_pose(frame, dst)
 
     else:
-        print("Not enough matches are found - %d/%d" % (len(matches),MIN_MATCH_COUNT))
+        print("Not enough matches are found - %d/%d" % (len(matches), MIN_MATCH_COUNT))
         matchesMask = None
 
 
-    draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+    draw_params = dict(matchColor = (255,255,0), # draw matches in green color
                        singlePointColor = None,
                        matchesMask = matchesMask, # draw only inliers
                        flags = 2)
 
-    img2 = cv2.drawMatches(img1,kp1,img2,kp2,matches,None,**draw_params)
+    frame = cv2.drawMatches(img1,kp1,frame,kp2,matches,None,**draw_params)
 
-    cv2.imshow('frame',img2)
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
