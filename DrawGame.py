7ded6e55368b92bee4e1ff400c3ac9a833b7e24f
Evelyn_H <manu.hobert@gmail.com>
2019-01-24 14:56:56 +0100

    .

--- DrawGame.py
+++ DrawGame.py
@@ -5,23 +5,31 @@ Created on Thu Jan 24 14:07:43 2019
 @author: heier
 """
 
+import sys
 import time
 from vision import Vision
+from ai.FormatConvert import FormatConvert, gridFile, crossFile
+
+
+
+if len(sys.argv) > 1 and sys.argv[1] == 'test':
+    from DrawClass import DrawTest
+    dt = DrawTest(None)
+    dt.testNN()
 
 
 class DrawGame:
     def __init__(self):
         print("Begginging drawing recognition game!")
         self.v = Vision()
-        
-    
+
     def run_game(self, times=3):
         current = 0
-        
+
         # Robot draws bounds.
-         self.arm.move_back()
-         FormatConvert.drawFromFile(boundFile, self.arm, speed=3)
-         self.arm.move_away()
+        self.arm.move_back()
+        FormatConvert.drawFromFile("bounds.txt", self.arm, speed=3)
+        self.arm.move_away()
         # Small wait
         time.sleep(1)
         # Do game loop
@@ -29,18 +37,18 @@ class DrawGame:
             # Human draws
             # Wait until human is "finished"
             input("Press enter when finished...")
-            
+
             # When human is finished, detect category
             img = self.flush_and_get()
-            
+
             # Get drawing of a category
-            
+
             # Robot draws the category
-            
+
             # Tiny wait
             time.sleep(1)
         print("Game over!")
-        
+
     def flush_and_get(self):
         img = self.v._getImage(self.v.cam1)
         img = self.v._getImage(self.v.cam1)
@@ -53,7 +61,5 @@ class DrawGame:
         img = self.v._getImage(self.v.cam1)
         img = self.v._getImage(self.v.cam1)
         img = self.v._getImage(self.v.cam1)
-        
+
         return img
-        
-        
